68502d249374f1e02bc5f90719ea69e4c335efa4
Evan Hesketh <evanhesketh@yahoo.ca>
2023-04-20 17:48:52 -0400

    update EditableTodo

--- productiv/src/EditableTodo.js
+++ productiv/src/EditableTodo.js
@@ -12,27 +12,33 @@ import TodoForm from "./TodoForm";
  * EditableTodoList -> EditableTodo -> { Todo, TodoForm }
  */
 
-function EditableTodo() {
+function EditableTodo({todo, update, remove}) {
+  const [isEditing, setIsEditing] = useState(false);
+
 
   /** Toggle if this is being edited */
-  function toggleEdit() { }
+  function toggleEdit() {
+    isEditing ? setIsEditing(false) : setIsEditing(true);
+   }
 
   /** Call remove fn passed to this. */
-  function handleDelete() { }
+  function handleDelete() {
+    remove(todo.id);
+   }
 
   /** Edit form saved; toggle isEditing and update in ancestor. */
-  function handleSave(formData) { }
+  function handleSave(formData) {
+    toggleEdit();
+    update(todo);
+   }
 
   return (
       <div className="EditableTodo">
 
-                EITHER
+                {isEditing
+                ? (<TodoForm handleSave={handleSave} initialFormData={todo} />)
 
-                <TodoForm />
-
-                OR
-
-                <div className="mb-3">
+                :(<div className="mb-3">
                   <div className="float-end text-sm-end">
                     <button
                         className="EditableTodo-toggle btn-link btn btn-sm"
@@ -46,7 +52,7 @@ function EditableTodo() {
                     </button>
                   </div>
                   <Todo />
-                </div>
+                </div>)}
 
       </div>
   );
